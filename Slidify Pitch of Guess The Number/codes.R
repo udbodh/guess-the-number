8ffc64b5a865a6774094ea18b207845c20636a0b
Udbodh Shrestha <udbodh@gmail.com>
2016-05-13 03:06:15 -0600

    Added files for Guess the Number

---
+++ Slidify Pitch of Guess The Number/codes.R
@@ -0,0 +1,10 @@
+#install and load devtools
+install.packages("devtools")
+library(devtools)
+
+#install and load Slidify
+install_github('slidify', 'udbodh')
+install_github('slidifyLibraries', 'udbodh')
+library(slidify)
+
+author("Guess_The_Number")
